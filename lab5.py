55045ffbc71208030919a6c01c4c40e1ba2988ed
gd744 <gonzalezdavid7761@gmail.com>
2021-05-13 12:13:51 -0700

    structured english

---
+++ lab5.py
@@ -0,0 +1,10 @@
+"""
+This application will be translating a sentence based on user inputs.
+A text file will be read, and then a list of strings will be created within the file.
+The captured list is then compared to a dictionary.
+
+We will be defining a function that splits a user's input into a set of strings
+A loop will then run through the array of strings and match the word with the translation dictionary.
+
+The full translated set will be print to the user.
+"""
